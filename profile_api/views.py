9bb12bbcedfe46defe46e2f0a615bf3cd3056edf
SuyogMahajan <mahajansuyog9322@gmail.com>
2024-01-28 17:25:25 +0530

    added user authentication with token

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import viewsets
 from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
+
 from profile_api import serializers
-# Create your views here.
 from profile_api import models
+from profile_api import permissions
+
 
 
 class HelloApiView(APIView):
@@ -93,3 +99,11 @@ class HelloViewSets(viewsets.ViewSet):
 class UserProfileViewset(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email')
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
