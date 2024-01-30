c10264442a7e112854d750e350edd065246b303e
SuyogMahajan <mahajansuyog9322@gmail.com>
2024-01-31 00:36:48 +0530

    implemented profile feed model , viewset and serilaizer

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -6,8 +6,10 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSets, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewset)
+router.register('feed', views.UserProfileFeedViewset)
 
 urlpatterns = [
 path('hello-view/',views.HelloApiView.as_view()),
+path('login/',views.UserLoginApiView.as_view()),
 path('', include(router.urls))
 ]
--- profile_api/views.py
+++ profile_api/views.py
@@ -6,7 +6,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from profile_api import serializers
 from profile_api import models
@@ -107,3 +107,18 @@ class UserProfileViewset(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
    """Handle creating user authentication tokens"""
    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewset(viewsets.ModelViewSet):
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    authentication_classes = (TokenAuthentication,)
+
+    permission_classes = (
+    permissions.UpdateOwnStatus,
+    IsAuthenticatedOrReadOnly,
+    )
+
+    def perform_create(self, serializer):
+        """Sets the user profile to logged in user"""
+        serializer.save(user_profile=self.request.user)
